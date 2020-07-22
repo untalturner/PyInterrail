c3fe33d6343208eae4c1030cb23ad3984bc713b8
Daniel Turner Cebriá <untalturner@gmail.com>
2020-07-22 23:26:46 +0200

    Corrections

--- PyInterrail/client.py
+++ PyInterrail/client.py
@@ -4,22 +4,28 @@ import requests
 
 from PyInterrail.stop_location import from_json
 
+# Constants
+URL = "https://api.eurail.com/timetable/location.name"
 
-def get_locations(name):
-    """
-    The method to call Interrail server for list of locations.
 
-    Parameters
-    ----------
-    name : str
-        the name of the location
+class Client:
+    """ The main class for the client """
 
-    Returns
-    -------
-    list
-        a list of StopLocations
-    """
+    def get_locations(name):
+        """
+        The method to call Interrail server for list of locations.
 
-    url = "https://api.eurail.com/timetable/location.name?input=" + str(name)
-    response = requests.get(url)
-    return from_json(response.json())
+        Parameters
+        ----------
+        name : str
+            the name of the location
+
+        Returns
+        -------
+        list
+            a list of StopLocations
+        """
+
+        params = {'input': name}
+        response = requests.get(URL, params)
+        return from_json(response.json())
